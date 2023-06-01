9c9552313b4e7f0359e580d05850af10beb49226
Carla <chernandezdia@uoc.edu.es>
2023-06-01 23:55:51 +0100

    Aplicando Tailwind a las paginas contacto e index

---
+++ dist/members.html.17108.v
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link href="index.52945c01.css" rel="stylesheet">
+    <title>PEC 3 - Herramientas HTML y CSS II</title>
+  </head>
+  <body>
+    <header class="container mx-auto flex flex-wrap justify-center py-3 mb-4 border-y">
+  
+</header>
+    <ul class="members">    
+        <li>
+            <div class="box">
+                <figure>
+                    <img src="hombre_uno.094fe4d8.png">
+                </figure>
+                <h3>Luis Maria Velez</h3>
+                <p>Luis Maria Velez empezó en el año 2014 en nuestro club como principiante con tan solo 20 años. A día de hoy su mejor puesto ha sido el 3er puesto en la clasificación Nacional.</p>
+                <time>Desde el 12/09/2014</time>
+            </div>
+        </li>
+        <li>
+            <div class="box">
+                <figure>
+                    <img src="hombre_dos.f45b2cc9.png">
+                </figure>
+                <h3>Baltasar Yague</h3>
+                <p>Baltasar Yague empezó en el año 2014 en nuestro club como senior con 28 años. A día de hoy su mejor puesto ha sido el 2º puesto en la clasificación Nacional.</p>
+                <time>Desde el 12/09/2014</time>
+            </div>
+        </li>
+        <li><div class="box">
+            <figure>
+                <img src="mujer_uno.11483faf.png">
+            </figure>
+            <h3>Olivia Menendez</h3>
+            <p>Olivia Menendez empezó en el año 2020 en nuestro club como principiante con 17 años. A día de hoy su mejor puesto ha sido el 7º puesto en la clasificación Provincial.</p>
+            <time>Desde el 12/09/2020</time>
+        </div></li>
+        <li><div class="box">
+            <figure>
+                <img src="mujer_dos.8dc6b6f9.png">
+            </figure>
+            <h3>Miren Pedraza</h3>
+            <p>Miren Pedraza empezó en el año 2010 en nuestro club como principiante con 40 años. A día de hoy su mejor puesto ha sido el 5º puesto en la clasificación Provincial.</p>
+            <time>Desde el 18/09/2010</time>
+        </div></li>
+        <li><div class="box">
+            <figure>
+                <img src="mujer_tres.446c898d.png">
+            </figure>
+            <h3>Sara Cortes</h3>
+            <p>Sara Cortes empezó en el año 2012 en nuestro club como principiante con 25 años. A día de hoy su mejor puesto ha sido el 4º puesto en la clasificación Provincial.</p>
+            <time>Desde el  05/07/2012</time>
+        </div></li>
+        <li><div class="box">
+            <figure>
+                <img src="mujer_cuatro.19744982.png">
+            </figure>
+            <h3>Noelia Nadal</h3>
+            <p>Noelia Nadal empezó en el año 2014 en nuestro club como principiante con 14 años. A día de hoy su mejor puesto ha sido el 6º puesto en la clasificación Nacional.</p>
+            <time>12/09/2014</time>
+        </div></li>
+    </ul>
+    <div class="container-fluid my-5">
+    <footer class="text-center text-white">
+        <div class="container">
+            <section class="mt-5">
+                <div class="row text-center d-flex justify-content-center pt-5">
+                    <div class="col-md-2">
+                        <h6 class="text-uppercase font-weight-bold">
+                            <a href="index.html" class="text-white">Inicio</a>
+                        </h6>
+                    </div>
+                    <div class="col-md-2">
+                        <h6 class="text-uppercase font-weight-bold">
+                            <a href="members.html" class="text-white">Miembros</a>
+                        </h6>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <div class="text-center p-3" style="background-color: #0003">&copy; Universitat Oberta de Catalunya
+        </div>
+    </footer>
+</div>
+    <script src="index.abdb1b70.js" defer=""></script><!-- do not remove this line! -->
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      margin: {
+        'm-30': '30px auto'
+      }
+    },
+  },
+}
